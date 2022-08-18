957d17f3d9366ef17a89ff5166e1f2dc1824199f
codigeek <pbarrera@codigeek.com>
2022-08-18 09:20:44 -0500

    Added services to fetch pokemon listing

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -9,14 +9,36 @@ export class PokemonService {
     try {
       const response = await axios.get('https://pokeapi.co/api/v2/pokemon', {
         params: {
-          limit: 10,
-          offset: 0
+          limit: quantity,
+          offset: quantity * (page - 1)
         }
       });
+      if( response?.data ){
+        response.data.avgHeight = 0;
+        response.data.avgWeight = 0;
+      }
       await Promise.all(response?.data?.results.map(async (pokemon: any) => {
         const contents = await axios.get(pokemon.url);
         pokemon.detail = contents?.data;
+        response.data.avgHeight = response.data.avgHeight + pokemon?.detail?.height;
+        response.data.avgWeight = response.data.avgWeight + pokemon?.detail?.weight;
+        // Set ability string
+        if (pokemon?.detail.abilities) {
+          if (pokemon?.detail.abilities.length > 1) {
+            pokemon.detail.abilities.forEach((ability: any) => {
+              if (!ability.is_hidden) {
+                if (pokemon?.detail?.ability_concat) {
+                  pokemon.detail.ability_concat = pokemon?.detail?.ability_concat + ", " + ability.ability?.name;
+                } else {
+                  pokemon.detail.ability_concat = ability.ability?.name;
+                }
+              }
+            })
+          }
+        }
       }));
+      response.data.avgHeight = Math.floor(response.data.avgHeight/response?.data?.results?.length);
+      response.data.avgWeight = Math.floor(response.data.avgWeight/response?.data?.results?.length);
       return response?.data;
     } catch (error) {
       throw error;
